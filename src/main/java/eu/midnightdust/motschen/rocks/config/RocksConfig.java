05fddec133222e0aeb4218567e66e665b4db7f8c
Martin Prokoph <motschen@midnightdust.eu>
2025-06-21 20:02:32 +0200

    port: Chase the Skies (1.21.6)

--- src/main/java/eu/midnightdust/motschen/rocks/config/RocksConfig.java
+++ src/main/java/eu/midnightdust/motschen/rocks/config/RocksConfig.java
@@ -53,6 +53,7 @@ public class RocksConfig extends MidnightConfig {
 
     @Entry(category = effects) public static boolean enablePolymerMode = true;
     @Entry(category = effects) public static boolean forcePolymerMode = false;
-    @Entry(category = effects, requiredMod = "factorytools", min = 0, max = 200, isSlider = true) public static int polymerViewDistance = 100;
+    @Condition(requiredModId = "factorytools")
+    @Entry(category = effects, min = 0, max = 200, isSlider = true) public static int polymerViewDistance = 100;
     @Entry(category = effects) public static List<String> biomeExclusions = new ArrayList<>();
 }
